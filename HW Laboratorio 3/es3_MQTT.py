75385fbf146429adcc8f54c5b0fa1b88eb21926d
Alessia27 <ale.sarritzu@gmail.com>
2022-06-06 13:41:18 +0200

    Update es3_MQTT.py

--- HW Laboratorio 3/es3_MQTT.py
+++ HW Laboratorio 3/es3_MQTT.py
@@ -1,5 +1,3 @@
-import ast
-
 import paho.mqtt.client as PahoMQTT
 import time
 import json
@@ -64,12 +62,9 @@ class MyMQTT:
     def myOnMessageReceived(self,paho_mqtt , userdata, message):
         # A new message is received
         topic = message.topic.split('/')
-        print(message)
-        msg = message.payload.decode('utf-8')
-        print(msg)
+        msg1 = str(message.payload.decode("utf-8"))
+        msg = json.loads(msg1)
         if topic[-1] == 'command':
-            msg = json.loads(str(msg))
-            print("OK")
             if "e" not in msg or "bn" not in msg:
                 return "Missing data!"
             if "n" not in msg["e"][0] or "t" not in msg["e"][0] or "v" not in msg["e"][0] or "u" not in msg["e"][0]:
@@ -78,7 +73,6 @@ class MyMQTT:
                 return "Invalid name."
             if msg["e"][0]["v"] not in [0, 1]:
                 return "Invalid led command."
-            print("OK")
             peripheral = msg["e"][0]["n"]
             value = msg["e"][0]["v"]
             print("L:" + str(value))
@@ -97,4 +91,4 @@ if __name__ == "__main__":
         json_data = json.dumps(MESSAGE).encode('utf-8')
         yun.myPublish("tiot/group14", json_data)
 
-# [{"bn": "Yun","e": [{"n": "led","t": null,"v": 1,"u": null}]}]
+# "{\"bn\": \"Yun\",\"e\": [{\"n\": \"led\",\"t\": null,\"v\": 1,\"u\": null}]}"
