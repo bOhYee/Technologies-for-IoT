299ab0f1b8343db4204d23a4371c5519feea62ae
Alessia27 <ale.sarritzu@gmail.com>
2022-06-10 12:27:52 +0200

    Update es2_client.py

--- HW Laboratorio 3/E02/es2_client.py
+++ HW Laboratorio 3/E02/es2_client.py
@@ -5,12 +5,12 @@ import time
 if __name__ == "__main__":
     http = urllib3.PoolManager()
     while True:
-        msg = input("Temperature: ")
-        try:
-            val = float(msg)
-        except:
-            print("Cannot convert to float")
-            continue
+        # retrieve temperature values from arduino
+        http = urllib3.PoolManager()
+        msg = http.request("GET", "http://127.0.0.1:8080/arduino/temperature")
+        json_msg = json.loads(str(msg))
+        val = json_msg["e"][0]["v"]
+
         data = {"bn": "Yun",
                 "e": [{
                     "n": "temperature",
