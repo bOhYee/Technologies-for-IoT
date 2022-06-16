3c145fd6b703df39471db65b881195b9fc6ef4d8
Alessia27 <ale.sarritzu@gmail.com>
2022-06-16 09:57:27 +0200

    Update es2_client.py

--- HW Laboratorio 3/E02/es2_client.py
+++ HW Laboratorio 3/E02/es2_client.py
@@ -23,5 +23,5 @@ if __name__ == "__main__":
             json_data = json.dumps(data).encode('utf-8')
             r = http.request('POST', 'http://127.0.0.1:8080/log', body=json_data, headers={'Content-Type': 'application/json'})
             print(str(r.status))
-        else
+        else:
             print("Unrecognized message", msg)
