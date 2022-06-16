b3016a38cdb63a54d26182bb25a2c8e8d2b5c8e1
Alessia27 <ale.sarritzu@gmail.com>
2022-06-16 10:56:55 +0200

    Update E03.py

--- SW Laboratorio 3/E03.py
+++ SW Laboratorio 3/E03.py
@@ -7,7 +7,6 @@ import json
 # Configuration constants
 RESOURCE_CATALOG_ADDRESS = "http://127.0.0.1:8080/"
 MSG_BROKER_ADDRESS = "localhost"
-BASE_TOPIC_PUB = "tiot/group14/command/"
 
 # Used to check that the connection to the MQTT Broker
 isConnected = False
@@ -24,6 +23,12 @@ def main():
     # Define the uuid of the client
     uuid_cl = str(uuid.uuid1())
 
+    # Create the MQTT client
+    c = PahoMQTT.Client(uuid_cl, True)
+    c.on_connect = on_connect
+    c.connect(MSG_BROKER_ADDRESS)
+    c.loop_start()
+
     req = requests.get(RESOURCE_CATALOG_ADDRESS)
     if req.status_code != 200:
         req.raise_for_status()
@@ -33,17 +38,18 @@ def main():
 
     # Define the service
     new_service = {
-        "bn" : uuid_cl,
-        "e":[{
+        "bn": "LedServiceGroup14",
+        "e": [{
+            "n": uuid_cl,
             "des": "Turn on/off LED",
-            "ep" : "LedService",
-            "t" : str(time.time())
+            "ep": "LedService",
+            "t": str(time.time())
         }]
     }
 
     # Define the header of the request
     header = {
-        "Content-Type" : "application/json"
+        "Content-Type": "application/json"
     }
 
     # Make public this service
@@ -59,14 +65,8 @@ def main():
     # Filter the list of devices through removing the ones who do not have a LED
     devices = json.loads(req.text)
     for dev in devices:
-        if "led" not in dev["e"][0]["res"] :
+        if "led" not in dev["e"][0]["res"]:
             devices.remove(dev)
-
-    # Create the MQTT client
-    c = PahoMQTT.Client(uuid_cl, True)
-    c.on_connect = on_connect
-    c.connect(MSG_BROKER_ADDRESS)
-    c.loop_start()
 
     while not isConnected:
         pass
@@ -92,14 +92,12 @@ def main():
             name = dev["bn"]
             dev_body = dev["e"][0]
             index_endpoint = dev_body["res"].index("led")
-            endpoint = dev["ep"][index_endpoint]
-
-            # Create the topic with the uuid and endpoint
-            topic = BASE_TOPIC_PUB + str(name) + "/" + str(endpoint)
+            endpoint = dev_body["ep"][index_endpoint]
 
             payload["bn"] = name
             payload["e"][0]["v"] = led_value
-            c.publish(topic, json.dumps(payload), 2)
+            print("Publishing " + str(payload) + " at " + endpoint)
+            c.publish(endpoint, json.dumps(payload), 2)
 
         # Create the package to send
         if led_value == 0:
