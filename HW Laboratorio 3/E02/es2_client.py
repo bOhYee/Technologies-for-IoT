bee38918e1d2658e5117b81eb7fbda89aff2f2cf
pestobest <45558483+pestobest@users.noreply.github.com>
2022-06-18 16:17:52 +0200

    Update es2_client.py

--- HW Laboratorio 3/E02/es2_client.py
+++ HW Laboratorio 3/E02/es2_client.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 						"u": "Celsius"
 					}]}
 			json_data = json.dumps(data).encode('utf-8')
-			r = http.request('POST', 'http://192.168.1.192:8989/log', body=json_data, headers={'Content-Type': 'application/json'}) //Check the machine's local IP and paste it in http://<IP>:8989/log
+			r = http.request('POST', 'http://192.168.1.192:8989/log', body=json_data, headers={'Content-Type': 'application/json'}) #Check the server machine's local IP and paste it in http://<IP>:8989/log
 			print(str(r.status))
 		else:
 			print("Unrecognized message", msg)
