5c5cdf01b3c6d0ca2e93f4d4b94977615bdfe41f
pestobest <45558483+pestobest@users.noreply.github.com>
2022-06-18 16:14:56 +0200

    Update es2_client.py

--- HW Laboratorio 3/E02/es2_client.py
+++ HW Laboratorio 3/E02/es2_client.py
@@ -3,25 +3,25 @@ import urllib3
 import time
 
 if __name__ == "__main__":
-    http = urllib3.PoolManager()
-    while True:
-        msg = input()
-        msg = msg.split(':')
-        if msg[0] == 'T':
-            try:
-                val = float(msg[1].strip())
-            except:
-                print('Cannot convert to float')
-                continue
-            data = {"bn": "Yun",
-                    "e": [{
-                        "n": "temperature",
-                        "t": time.time(),
-                        "v": val,
-                        "u": "Celsius"
-                    }]}
-            json_data = json.dumps(data).encode('utf-8')
-            r = http.request('POST', 'http://127.0.0.1:8080/log', body=json_data, headers={'Content-Type': 'application/json'})
-            print(str(r.status))
-        else:
-            print("Unrecognized message", msg)
+	http = urllib3.PoolManager()
+	while True:
+		msg = input()
+		msg = msg.split(':')
+		if msg[0] == 'T':
+			try:
+				val = float(msg[1].strip())
+			except:
+				print('Cannot convert to float')
+				continue
+			data = {"bn": "Yun",
+					"e": [{
+						"n": "temperature",
+						"t": time.time(),
+						"v": val,
+						"u": "Celsius"
+					}]}
+			json_data = json.dumps(data).encode('utf-8')
+			r = http.request('POST', 'http://192.168.1.192:8989/log', body=json_data, headers={'Content-Type': 'application/json'}) //Check the machine's local IP and paste it in http://<IP>:8989/log
+			print(str(r.status))
+		else:
+			print("Unrecognized message", msg)
