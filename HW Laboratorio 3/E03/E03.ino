8fbee44c907d411ad1283ffccae17291978d6b68
pestobest <45558483+pestobest@users.noreply.github.com>
2022-06-19 22:58:35 +0200

    Update E03.ino

--- HW Laboratorio 3/E03/E03.ino
+++ HW Laboratorio 3/E03/E03.ino
@@ -2,14 +2,12 @@
 #include<Process.h>
 #include <math.h>
 
-// LED variables
-const int LED_PIN = 10;
-
 // Temperature sensor variables
 const int TEMP_PIN = A1;
 const float B = 4275;
 const float R0 = 100000;
 const int WAIT_TIME = 2000;
+const int LED_PIN = 10;
 Process proc;
 
 float calculateTemperature(int v_read){
@@ -23,21 +21,41 @@ float calculateTemperature(int v_read){
     return temperature;
 }
 
-void processCommand(String command){
-  command.trim();
-  if (command.startsWith("L:")== true)
-    {
-      if (command.endsWith("1")== true)
-        analogWrite(LED_PIN,HIGH);
-      if (command.endsWith("0")== true)
-        analogWrite(LED_PIN,LOW);
-      Serial.println(command);
-      }
-   else {
-    Serial.print("Error: ");
-    Serial.println(command);
+void processCommand(){
+    char ret;
+    String cmd;
+    while(proc.available() > 0){
+        ret = proc.read();
+        cmd = cmd + String(ret);
     }
-  
+    Serial.print("Received command: ");
+    Serial.print(cmd);
+    
+    if(cmd.indexOf("L:") >= 0){
+        cmd = cmd.charAt(cmd.indexOf("L:")+2);
+        if (cmd == "1" || cmd == "0"){
+            digitalWrite(LED_PIN, cmd.toInt());
+        }
+    }
+    else if(cmd.indexOf("E:") >= 0){
+        cmd = cmd.charAt(cmd.indexOf("E:")+2);
+        if(cmd == "1"){
+            Serial.println("Cannot convert to float");
+        }
+        else if(cmd == "2"){
+            Serial.println("Bad request: command syntax \'T:<temperature>\'");
+        }
+        else if(cmd == "3"){
+            Serial.println("Bad request: missing data");
+        }
+        else if(cmd == "4"){
+            Serial.println("Bad request: invalid name");
+        }
+        else if(cmd == "5"){
+            Serial.println("Bad request: command syntax \'L:<1, 0>\'");
+        }        
+    }
+    return;
 }
 
 void setup(){
@@ -47,10 +65,7 @@ void setup(){
     Serial.println("Initializing Bridge connection...");
     Bridge.begin();
     Serial.println("Bridge connection established.");
-    
-    pinMode(LED_PIN,OUTPUT);
-    analogWrite(LED_PIN,LOW);
-    
+
     proc.begin("python3");
     proc.addParameter("/root/E03_Lin.py");
     proc.runAsynchronously();
@@ -58,20 +73,14 @@ void setup(){
 
 void loop(){
     int v_read = analogRead(TEMP_PIN);
-    char ret;
-    float temp;
-    String cmd;
-
-    temp = calculateTemperature(v_read);
-    String msg = "T:" + String(temp);
-    proc.println(msg);
+    float temp = calculateTemperature(v_read);
+    String res = "T:" + String(temp);
+    Serial.print("Sent command: ");
+    Serial.println(res);
+    proc.println(res);
 
     while(proc.available() > 0){
-        ret = proc.read();
-        cmd+=ret;
-        processCommand(cmd);
-        delay(50);
-        cmd[0]='\0';
+        processCommand();
     }
 
     delay(WAIT_TIME);
